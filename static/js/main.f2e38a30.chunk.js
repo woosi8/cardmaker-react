9d04c19a13adf4732d8598d194aef67c6c051ea8
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-02-24 19:56:58 +0900

    Updates

---
+++ static/js/main.f2e38a30.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptemplate=this.webpackJsonptemplate||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n.n(c),a=n(3),o=n.n(a),s=(n(8),n(9),n(1));var i=function(){return Object(s.jsx)("h1",{children:"Hello card"})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){e.exports={makerBlack:"black"}},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.f2e38a30.chunk.js.map
