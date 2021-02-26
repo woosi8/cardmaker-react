17ab09d1ff5d8479e15a37ce4d367b94d00d50a0
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-02-26 22:56:40 +0900

    add login component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import style from "./header.module.css";
 
-const Header = (props) => <h1>Header</h1>;
+const Header = (
+	{ onLogout } //
+) => (
+	<header className={style.header}>
+		{onLogout && (
+			<button className={style.header} onClick={onLogout}>
+				Logout
+			</button>
+		)}
+		<img
+			className={style.logo}
+			src={process.env.PUBLIC_URL + "/images/logo.png"}
+			alt="logo"
+		/>
+
+		<h1 className={style.title}>Business Card Maker</h1>
+	</header>
+);
 
 export default Header;
