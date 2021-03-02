9654ba6158832d39714a470f60ce9a8290a3cb6f
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-03-02 13:03:42 +0900

    make card editor,preview

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
@@ -7,6 +7,38 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+	const [cards, setCards] = useState([
+		{
+			id: "1",
+			company: "Samsung",
+			theme: "light",
+			title: "Software Engineer",
+			email: "tmfvmehek@gmail.com",
+			message: "go for it",
+			firstName: "ellie1",
+			fileURL: "",
+		},
+		{
+			id: "2",
+			company: "Samsung",
+			theme: "light",
+			title: "Software Engineer",
+			email: "tmfvmehek@gmail.com",
+			message: "go for it",
+			firstName: "ellie2",
+			fileURL: "",
+		},
+		{
+			id: "3",
+			company: "Samsung",
+			theme: "light",
+			title: "Software Engineer",
+			email: "tmfvmehek@gmail.com",
+			message: "go for it",
+			firstName: "ellie3",
+			fileURL: "",
+		},
+	]);
 	const history = useHistory();
 	const onLogout = () => {
 		authService.logout();
@@ -23,8 +55,8 @@ const Maker = ({ authService }) => {
 		<section className={styles.maker}>
 			<Header onLogout={onLogout} />
 			<div className={styles.container}>
-				<Editor />
-				<Preview />
+				<Editor cards={cards} />
+				<Preview cards={cards} />
 			</div>
 			<Footer />
 		</section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Card from "../card/card";
 import styles from "./preview.module.css";
 
-const Preview = (props) => (
+const Preview = ({ cards }) => (
 	<section className={styles.preview}>
 		<h1 className={styles.title}>Card Preview</h1>
+		{/* cards를 돌면서 각각의 카드를 해당하는 컴포넌트로 변환 */}
+		{cards.map((card) => (
+			<Card card={card} />
+		))}
 	</section>
 );
 
