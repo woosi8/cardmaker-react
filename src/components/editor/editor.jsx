9fd411aa179f12b756d5d3c23a7fbb33e59eaa34
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2021-05-04 15:14:33 +0900

    modified editor h1 name

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -4,7 +4,7 @@ import styles from "./editor.module.css";
 
 const Editor = ({ FileInput, cards, addCard, updateCard, deleteCard }) => (
 	<section className={styles.editor}>
-		<h1 className={styles.title}>Card Maker</h1>
+		<h1 className={styles.title}>Card Editor</h1>
 		{/* { {cards.map((card) => (  cards는 더이상 배열이 아닌 오브젝트이기 때문에} */}
 		{/* 오브젝트를 맵핑해줄때 쓰는 Object.keys라는 함수를 이용 , cards안에 있는 모든 key를 받아온다(문자열로) */}
 		{Object.keys(cards).map((
